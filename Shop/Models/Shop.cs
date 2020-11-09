48dd419c316cdee9a1d67f3c4a6ea321b0796277
p_an <vnhomesoft@gmail.com>
2020-11-09 08:32:21 -0800

    Project init

---
+++ Shop/Models/Shop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shop.Models
+{
+    public class Shop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ Shop/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shop.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string LoginName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
